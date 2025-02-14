b5df4b42e4e4dbd6eebab96652faf50c4dea93ff
Wolf3s <andregui17@outlook.com>
2025-02-14 07:32:01 -0300

    Bitmap: replace posix calls to c calls.

--- d_bitmap.c
+++ d_bitmap.c
@@ -8,32 +8,71 @@
 ** License: Public domain/ID License 
 */
 
+#include "doomtype.h"
 #include "d_bitmap.h"
 #include <stdlib.h>
 
+/*
+** BMP File header structure definition cleanuped from Windows Api.
+** Main Changes:
+** Removed typedefs of the windows api
+** Fixed some wrong types used on the structures
+*/
+
+typedef struct {
+    unsigned short  type;
+    size_t size;
+    unsigned short  reserved1;
+    unsigned short  reserved2;
+    size_t offBits;
+} bitmapfile_t;
+
+typedef struct {
+    size_t size;
+    long  width;
+    long  height;
+    unsigned short  planes;
+    unsigned short  bitCount;
+    size_t compression;
+    size_t sizeImage;
+    long  xPelsPerMeter;
+    long  yPelsPerMeter;
+    size_t clrUsed;
+    size_t clrImportant;
+} bitmapinfo_t;
+
+typedef struct {
+    unsigned char blue;
+    unsigned char green;
+    unsigned char red;
+    unsigned char reserved;
+} rgbquad_t;
+
 void D_LoadBmp(dbyte* rgb, const char* filename, int width, int height)
 {
-    int fn, s, d;
+    FILE *fn;
+    int s, d;
     unsigned char* texels;
     bitmapfile_t bmfh;
     bitmapinfo_t bmi;
     rgbquad_t* bmpPalette;
 
-    fn = Open(filename, O_RDONLY | O_BINARY);
+    fn = fopen(filename, "rb");
 
-    Read(fn, &bmfh, sizeof(bitmapfile_t));
-    Read(fn, &bmi, sizeof(bitmapfile_t));
+    fread(&bmfh, sizeof(bitmapfile_t), 1, fn);
+    fread(&bmi, sizeof(bitmapinfo_t), 1, fn);
 
     bmi.width = width;
     bmi.height = height;
 
     bmpPalette = (rgbquad_t*)malloc(sizeof(rgbquad_t) * 256);
-    Read(fn, bmpPalette, (sizeof(rgbquad_t) * 256));
+    fread(bmpPalette, sizeof(rgbquad_t) * 256, 1, fn);
 
     texels = (unsigned char*)malloc(width * height);
-    LSeek(fn, (long)bmfh.offBits, SEEK_SET);
-    Read(fn, texels, (width * height));
-    Close(fn);
+    fseek(fn, bmfh.offBits, SEEK_SET);
+    fread(texels, (width * height), 1, fn);
+
+    fclose(fn);
 
     rgb = (dbyte*)malloc(width * (height * 3));
     for (s = 0, d = 0; s < (width * height); s++)
